e8e9bfecd816e8d15c266a40bad6c4981621bf44
Michael Klem <michael.klem@gmail.com>
2021-11-28 03:42:50 +0000

    Able to call ffprobe

--- modules/Program.js
+++ modules/Program.js
@@ -4,6 +4,8 @@ var Logger = require('./Logger');
 var logger = Logger.logger();
 const config = require('../config.json');
 const fs = require('fs');
+const spawn = require('await-spawn')
+
 
 class Program {
 
@@ -53,11 +55,19 @@ class Program {
       this.imageCount = 0;
 
       try {
-        this.dm.startProcessing( story.data.story_id )
+        await this.dm.startProcessing( story.data.story_id )
         this.story_id = story.data.story_id
         this.user_id = story.data.user_id
         this.setStoryDirectories()
         this.createDirectories()
+        // if (fs.existsSync(this.firstImagePath)) {
+          // not using this code
+        // }
+
+        if (fs.existsSync(this.lastImagePath)) {
+          await this.createAudioFile(null, this.tempAudioDirectoryPrefix+ "last" + story.story_id + ".flv");
+          // this.createCopies(this.lastImagePath, 0);
+        }
       }
       catch(err) {
         logger.info(`[Program.compileVideo] Error: ${err}`)
@@ -76,7 +86,7 @@ class Program {
       this.storyVideoDirectoryPrefix = this.storyDirectoryPrefix + "video/";
       logger.info('[setStoryDirectories] exists test: ' + this.storyVideoDirectoryPrefix)
       if (! fs.existsSync(this.storyVideoDirectoryPrefix)) {
-        fs.mkdirSync(this.storyVideoDirectoryPrefix)
+        fs.mkdirSync(this.storyVideoDirectoryPrefix, { recursive: true })
         logger.info('Created directory: ' + this.storyVideoDirectoryPrefix)
       }
     }
@@ -101,29 +111,88 @@ class Program {
       }
 
       if (! fs.existsSync(this.tempUserDirectoryPrefix)) {
-        fs.mkdirSync(this.tempUserDirectoryPrefix)
+        fs.mkdirSync(this.tempUserDirectoryPrefix, { recursive: true })
         logger.info('[createDirectories] Created user directory: ' + this.tempUserDirectoryPrefix)
       }
 
       if (! fs.existsSync(this.tempStoryDirectoryPrefix)) {
-        fs.mkdirSync(this.tempStoryDirectoryPrefix)
+        fs.mkdirSync(this.tempStoryDirectoryPrefix, { recursive: true })
         logger.info('[createDirectories] Created story directory: ' + this.tempStoryDirectoryPrefix)
       }
 
       if (! fs.existsSync(this.tempImagesDirectoryPrefix)) {
-        fs.mkdirSync(this.tempImagesDirectoryPrefix)
+        fs.mkdirSync(this.tempImagesDirectoryPrefix, { recursive: true })
         logger.info('[createDirectories] Created images directory: ' + this.tempImagesDirectoryPrefix)
       }
 
       if (! fs.existsSync(this.tempAudioDirectoryPrefix)) {
-        fs.mkdirSync(this.tempAudioDirectoryPrefix)
+        fs.mkdirSync(this.tempAudioDirectoryPrefix, { recursive: true })
         logger.info('[createDirectories] Created audio directory: ' + this.tempAudioDirectoryPrefix)
       }
     }
     catch(err){
       logger.info('[createDirectories] Error: ' + err)    
     }
+  } // create directories
+
+  async createAudioFile(sourcePath, destPath) {
+    try {
+      if (sourcePath != null && sourcePath != "") {
+        logger.info('[createAudioFile] sourcePath: ' + sourcePath);
+        let duration = await this.getDuration(sourcePath);
+        if (duration === 0) {
+          sourcePath = this.emptyAudio;
+        }
+      }
+      else
+      {
+        sourcePath = this.emptyAudio;
+        let duration = await this.getDuration(sourcePath);
+
+      }
+
+      // Copy the source to the temp directory to be worked on
+      logger.info(`[createAudioFile] copying ${sourcePath} to ${destPath}`);
+      fs.copyFileSync(sourcePath, destPath);
+      this.audioFiles.push(destPath)
+    }
+    catch(err){
+      logger.info('[createAudioFile] Error: ' + err)
+    }
   }
+
+  // call system command to get duration:
+  // ffprobe -i testAudio.flv -show_entries format=duration -v quiet -of csv="p=0"
+  // ffprobe -v quiet -print_format compact=print_section=0:nokey=1:escape=csv -show_entries format=duration "/opt/mp4_utility/testAudio.flv"
+
+  // which returns duration in seconds 5.434000 for 5.43 seconds
+  async getDuration(filePath) {
+    let duration = 0;
+
+    try {
+      logger.info('[getDuration] filePath: ' + filePath)
+
+      let cmd = config.ffprobe_commandline;
+      cmd = cmd.replace('%input_file%', filePath);
+
+      logger.info('[getDuration] cmd: ' + cmd);
+      let args = [
+          '-i', filePath,
+          '-v','quiet',
+          '-print_format','compact=print_section=0:nokey=1:escape=csv',
+          '-show_entries', 'format=duration'
+      ];
+
+      duration = await spawn('/usr/bin/ffprobe', args);
+      console.log('[getDuration] duration: ' + duration)
+      return duration
+    } 
+    catch (e) {
+      console.log('[getDuration] Error: ' + e.stderr.toString())
+      throw e
+    }
+  }
+  
 }
 
 module.exports = Program;
